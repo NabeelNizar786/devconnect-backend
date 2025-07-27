d17eb18fd1df4b9f751835dc1bc20c2da5fa8a75
nabeel nizar <nabeelnizar786@gmail.com>
2025-07-27 20:56:07 +0530

    database configured

--- src/app.js
+++ src/app.js
@@ -1,23 +1,38 @@
 const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
 const PORT = 3000;
-const { isAdminAuth } = require("./middlewares/auth");
 
-app.use("/admin", isAdminAuth);
+app.post("/signup", async (req, res) => {
+  const user = new Use({
+    firstName: "Rajesh",
+    lastName: "Kaloji",
+    emailId: "rajeshkaloji@gmail.com",
+    password: "123456",
+  });
 
-app.get("/admin/getAllUsers", (req, res) => {
   try {
-    throw new Error("err");
-  } catch (error) {
-    res.status(500).send("Error");
-  }
-  res.send("data send");
-});
-
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("Something went wrong");
+    let result = await user.save();
+    res.status(200).send({ message: "User Added Successfully", result });
+  } catch (err) {
+    res.status(404).send("Error While Creating User");
   }
 });
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+connectDB()
+  .then(() => {
+    console.log("Database Connection Established ...!");
+    app.listen(PORT, () => {
+      console.log("Server Connection Started Succesfully");
+    });
+  })
+  .catch((err) => {
+    console.error("Database Connection Failed!");
+  });
+
+  app.use("/", (err, req, res, next) => {
+    if (err) {
+      res.send("Error:" + err.message);
+    }
+  });
