fba9d49593fb5a63e9666276ca660e17c9a8ac09
nabeel nizar <nabeelnizar786@gmail.com>
2025-11-30 18:48:38 +0530

    Added connection request model and implemented profile editing with validation

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfileData } = require("../utils/validation");
 
 const profileRouter = express.Router();
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     let userInfo = req.user;
     res.status(200).send(userInfo);
+  } catch (error) {
+    res.status(400).send("ERROR:" + error.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Edit Request");
+    }
+
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+
+    await loggedInUser.save();
+
+    return res
+      .status(200)
+      .json({
+        message: `${loggedInUser.firstName} your profile has been updated successfully`,
+        data: loggedInUser,
+      });
   } catch (error) {
     res.status(400).send("ERROR:" + error.message);
   }
--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,14 +1,68 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequestModel = require("../models/connectionRequest");
+const user = require("../models/user");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  try {
-    res.status(200).send("request sent successful!");
-  } catch (error) {
-    res.status(400).send("ERROR:" + error.message);
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const allowedStatus = ["ignored", "interested"];
+
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status value: " + status });
+      }
+
+      const toUser = await user.findById(toUserId);
+
+      if (!toUser) {
+        return res
+          .status(404)
+          .json({
+            message: "The user you are trying to connect with does not exist.",
+          });
+      }
+
+      const existingConnectionRequest = await ConnectionRequestModel.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (existingConnectionRequest) {
+        return res
+          .status(400)
+          .json({
+            message: "Connection request already exists between these users.",
+          });
+      }
+
+      const ConnectionRequest = await ConnectionRequestModel({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await ConnectionRequest.save();
+
+      res.status(200).json({
+        message: `${req.user.firstName} is ${status} in ${toUser.firstName}`,
+        data: data,
+      });
+    } catch (error) {
+      res.status(400).send("ERROR:" + error.message);
+    }
   }
-});
+);
 
 module.exports = requestRouter;
