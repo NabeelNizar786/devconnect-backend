be7cb358ec934e15391a07951b568d4b26bf5be7
nabeel nizar <nabeelnizar786@gmail.com>
2026-01-01 20:42:47 +0530

    fix: include password in USER_SAFE_DATA for user connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,7 +4,7 @@ const ConnectionRequestModel = require("../models/connectionRequest");
 const user = require("../models/user");
 const userRouter = express.Router();
 
-const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+const USER_SAFE_DATA = "firstName lastName password photoUrl age gender about skills";
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
