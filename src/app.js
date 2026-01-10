6b86deb74b19e90d161384bbe8bcdc6b76baedf6
nabeel nizar <nabeelnizar786@gmail.com>
2026-01-10 14:32:17 +0530

    feat: integrate Razorpay payment gateway and add payment processing routes

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
-require('dotenv').config()
+require("dotenv").config();
 
 app.use(
   cors({
@@ -21,79 +21,13 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/requests");
 const userRouter = require("./routes/user");
+const paymentRouter = require("./routes/payment");
 
 app.use("/", authRouter),
   app.use("/", profileRouter),
   app.use("/", requestRouter);
 app.use("/", userRouter);
-
-app.get("/user", async (req, res) => {
-  const userEmail = req.body.emailId;
-
-  try {
-    const user = await User.findOne({ emailId: userEmail });
-
-    if (user.length === 0) {
-      res.status(404).send({ message: "User Not Found!" });
-    }
-    res.status(200).send({ message: "User Found", user });
-  } catch (error) {
-    res.status(404).send("Error while fetching user");
-  }
-});
-
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.status(200).send({ message: "User List", users });
-  } catch (error) {
-    res.status(404).send("Error while fetching user");
-  }
-});
-
-app.delete("/delete", async (req, res) => {
-  const userId = req.body.userId;
-  try {
-    let result = await User.findByIdAndDelete(userId);
-    res.status(200).send({ message: "User Successfully Deleted!" }, result);
-  } catch (error) {
-    res.status(500).send({ message: "Error while deleting user", error });
-  }
-});
-
-app.patch("/update/:userId", async (req, res) => {
-  const userId = req.params.userId;
-  const data = req.body;
-
-  try {
-    const ALLOWED_UPDATES = [
-      "password",
-      "photoUrl",
-      "about",
-      "gender",
-      "age",
-      "skills",
-    ];
-    const isUpdateAllowed = Object.keys(data).every((k) =>
-      ALLOWED_UPDATES.includes(k)
-    );
-    if (!isUpdateAllowed) {
-      throw new Error("Update not allowed");
-    }
-
-    if (req.body?.skills.length > 5) {
-      throw new Error("skills must be fewer or equal to 5");
-    }
-    let result = await User.findByIdAndUpdate({ _id: userId }, data, {
-      returnDocument: "before",
-      runValidators: true,
-    });
-    res.status(200).send({ message: "User successfully updated!", result });
-  } catch (err) {
-    let error = err.message;
-    res.status(500).send({ message: "Error while Updating user", error });
-  }
-});
+app.use("/", paymentRouter);
 
 connectDB()
   .then(() => {
---
+++ src/utils/constant.js
@@ -0,0 +1,7 @@
+const membershipAmount = {
+  Silver: "700",
+  Gold: "900",
+  Platinum: "1200",
+};
+
+module.exports = { membershipAmount };
